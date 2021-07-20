2187c2c6a23fdaa82a74f762af5c755209b6782a
ModXPal <collas.robin@gmail.com>
2021-07-20 16:07:58 +0200

    printf pour la vie

--- Makefile
+++ Makefile
@@ -6,8 +6,14 @@ SRC_FILES=		srcs/printf.c \
 				srcs/printf_utils_part1.c \
 				srcs/printf_utils_part2.c \
 				srcs/printf_utils_part3.c
+BONUS=			srcs/printf_bonus.c \
+			srcs/printf_utils_part1_bonus.c \
+			srcs/printf_utils_part2_bonus.c \
+			srcs/printf_utils_part3_bonus.c
 
 SRC_OBJS=		${SRC_FILES:.c=.o}
+
+BONUS_OBJS=		$(BONUS:.c=.o)
 
 CC= 			gcc
 
@@ -17,13 +23,19 @@ RM=				rm -f
 
 AR=				ar rcs
 
+FILE=				touch
+
 all:			${NAME}
 
 ${NAME}:    	${SRC_OBJS}
 				${AR} ${NAME} ${SRC_OBJS}
 
+bonus:		$(BONUS_OBJS)
+				$(AR) $(NAME) $(BONUS_OBJS)
+				$(FILE) bonus
+
 clean:
-				${RM} ${SRC_OBJS}
+				${RM} ${SRC_OBJS} $(BONUS_OBJS) bonus
 
 fclean: 		clean
 				${RM} ${NAME}
--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 16:16:55 by rcollas           #+#    #+#             */
-/*   Updated: 2021/07/03 11:00:29 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/07/20 16:00:34 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_printf.h"
+# define UINT_MAX 4294967295
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define CHAR_MAX 127
+# define CHAR_MIN -128
 
 int	main(void)
 {
@@ -28,6 +33,7 @@ int	main(void)
 
 	ptr = &num;
 	nb_un = 4294967295;
+	ft_printf("Salut tout le monde !\n");
 	/*
 	printf("test rp %%010.2d --> |%010.2d| \n", 5);
 	ft_printf("test mp %%010.2d --> |%010.2d| \n", 5);
@@ -59,7 +65,6 @@ int	main(void)
 	ft_printf("test mp %%.3s -----------> |%.3s|\n", NULL);
 	printf("test rp %%4.3s ----------> |%4.3s|\n", print);
 	ft_printf("test mp %%4.3s ----------> |%4.3s|\n", print);
-	*/
 	printf("test rp %%d ------------> |%d| \n", 0);
 	ft_printf("test mp %%d ------------> |%d| \n", 0);
 	printf("test rp %%.d ------------> |%.d| \n", 0);
@@ -120,9 +125,50 @@ int	main(void)
 	ft_printf("test mp UINT_MAX+1 %%09.2d -> |%09.2d| \n", 4294967295 + 1); 
 	printf("test rp UINT_MAX+1 %%09.0d -> |%09.0d| \n", 4294967295 + 1); 
 	ft_printf("test mp UINT_MAX+1 %%09.0d -> |%09.0d| \n", 4294967295 + 1); 
-	printf("test rp CHAR_MAX CHAR_MIN --0*%%-0*.2d*0 0*%%-0*.2d*0-- --> |--0*%-0*.2d*0 0*%-0*.2d*0--| \n", -21, 127, 21, -128); 
-	ft_printf("test mp CHAR_MAX CHAR_MIN --0*%%-0*.2d*0 0*%%-0*.2d*0-- --> |--0*%-0*.2d*0 0*%-*d*0--| \n", -21, 127, 21, -128); 
+	printf("test rp %%-3.2u %%10.42u 1 -1 ----> |%-3.2u %10.42u| \n", 1, -1);
+	ft_printf("test mp %%-3.2u %%10.42u 1 -1 ----> |%-3.2u %10.42u| \n", 1, -1);
+	printf("test rp UINT_MAX %%9.0u ----------> |%9.0u| \n", 4294967295);
+	ft_printf("test mp UINT_MAX %%9.0u ----------> |%9.0u| \n", 4294967295);
+	printf("test rp %%*c%%*c ---------> |%*c%*c| \n", -10, '0', 10, '1');
+	ft_printf("test mp %%*c%%*c ---------> |%*c%*c| \n", -10, '0', 10, '1');
+	printf("test rp %%x 0 ----> |%x| \n", 0);
+	ft_printf("test mp %%x 0 ----> |%x| \n", 0);
+	printf("test rp %%x -1 ----> |%x| \n", -1);
+	ft_printf("test mp %%x -1 ----> |%x| \n", -1);
+	printf("test rp %%x -10 ----> |%x| \n", -10);
+	ft_printf("test mp %%x -10 ----> |%x| \n", -10);
+	printf("test rp %%p %%p LONG_MIN LONG_MAX ----> |%p %p| \n", -2147483648, 2147483647);
+	ft_printf("test mp %%p %%p LONG_MIN LONG_MAX ----> |%p %p| \n", -2147483648, 2147483647);
+	printf("test rp %%p %%p 0 0 -----> |%p %p| \n", 0, 0);
+	ft_printf("test mp %%p %%p 0 0 -----> |%p %p| \n", 0, 0);
+	printf("retour rp = %d ||| \n", printf("test rp %%p %%p 0 0 -----> |%p %p| \n", NULL, NULL));
+	ft_printf("retour mp = %d ||| \n", ft_printf("test mp %%p %%p 0 0 -----> |%p %p| \n", NULL, NULL));
+	printf("test rp %%-2p -1 ---> |%-2p| \n", -1);
+	ft_printf("test mp %%-2p -1 ---> |%-2p| \n", -1);
+	printf("retour rp = %d ||| \n", printf("test rp %%-3p 16 ---> |%-3p| \n", 16));
+	ft_printf("retour mp = %d ||| \n", ft_printf("test mp %%-3p 16 ---> |%-3p| \n", 16));
+	printf("test rp %%-9p %%-10p LONG_MIN_MAX ---> |%-9p %-10p| \n", -9223372036854775808, 9223372036854775807);
+	ft_printf("test mp %%-9p %%-10p LONG_MIN_MAX ---> |%-9p %-10p| \n", -9223372036854775808, 9223372036854775807);
+	*/
+	printf("retour rp = %d \n", printf("test rp %%-15x LONG_MAX ---> |%-11x| \n", 9223372036854775807LL));
+	printf("retour mp = %d \n", ft_printf("test mp %%-15x LONG_MAX ---> |%-11x| \n", 9223372036854775807LL));
 	/*
+	printf("test rp CHAR_MAX CHAR_MIN --0*%%-0*.2d*0 0*%%-0*.2d*0-- --> |--0*%-0*.2d*0 0*%-0*.2d*0--| \n", -21, 127, 21, -128); 
+	ft_printf("test mp CHAR_MAX CHAR_MIN --0*%%-0*.2d*0 0*%%-0*.2d*0-- --> |--0*%-0*.2d*0 0*%-0*.2d*0--| \n", -21, 127, 21, -128); 
+	printf("test rp 0*%%0-*d*0 0*%%0*d*0 ----> |0*%0-*d*0 0*%0*d*0| \n", 21, 1021, 21, -1011);
+	ft_printf("test mp 0*%%0-*d*0 0*%%0*d*0 ----> |0*%0-*d*0 0*%0*d*0| \n", 21, 1021, 21, -1011);
+	printf("test rp UINT_MAX %%10.10d -------> |%10.10d| \n", 4294967295);
+	ft_printf("test mp UINT_MAX %%10.10d -------> |%10.10d| \n", 4294967295);
+	printf("test rp %%3d 0 ------------------> |%3d| \n", 0);
+	ft_printf("test mp %%3d 0 ------------------> |%3d| \n", 0);
+	printf("test rp %%0*d -7 -54 ------------> |%0*d| \n", -7, -54);
+	ft_printf("test mp %%0*d -7 -54 ------------> |%0*d| \n", -7, -54);
+	printf("test rp %%03d 0 -----------------> |%03d| \n", 0);
+	ft_printf("test mp %%03d 0 -----------------> |%03d| \n", 0);
+	printf("test rp %%3d 0 -----------------> |%3d| \n", 0);
+	ft_printf("test mp %%3d 0 -----------------> |%3d| \n", 0);
+	printf("test rp %%1.1d ------------------> |%1.1d| \n", 0);
+	ft_printf("test rp %%1.1d ------------------> |%1.1d| \n", 0);
 	printf("test rp %%i hexa---------> %i|\n", ici);
 	ft_printf("test mp %%d hexa---------> %d|\n", ici);
 	ft_printf("test mp %%i hexa---------> %i|\n", ici);
